e0be61a861efb80c03c0a35cf552a977a34eb6a1
Matt Chun-Lum <mchunlum@gmail.com>
2014-08-06 21:15:26 -0700

    ball bouncing

--- breakout/breakout.py
+++ breakout/breakout.py
@@ -6,10 +6,35 @@ from pygame.sprite import Sprite
 pygame.init()
 
 class Ball(Sprite):
+  def __init__(self, x, y, radius, color=(0, 0, 0)):
+    Sprite.__init__(self)
+    self.image = pygame.Surface((radius * 2, radius * 2))
+    self.image.fill((255, 255, 255))
+    pygame.draw.circle(self.image, color, (radius, radius), radius)
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.vx = 5
+    self.vy = 5
+
+  def bounceX(self):
+    self.vx = -self.vx
+
+  def bounceY(self):
+    self.vy = -self.vy
+
+  def update(self):
+    self.rect.x += self.vx
+    self.rect.y += self.vy
+
+class Paddle(Sprite):
   def __init__(self, x, y, size, color):
     Sprite.__init__(self)
-    self.x = x
-    self.y = y
+    self.image = pygame.Surface(size)
+    self.image.fill(color)
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
 
 class Brick(Sprite):
   def __init__(self, x, y, size, color):
@@ -38,7 +63,7 @@ class Breakout:
   BRICK_SEP       = 4
   BRICK_WIDTH     = (WIDTH - (BRICKS_PER_ROW - 1) * BRICK_SEP) / BRICKS_PER_ROW
   BRICK_HEIGHT    = 8
-  BRICK_SIZE      = [BRICK_WIDTH, BRICK_HEIGHT]
+  BRICK_SIZE      = (BRICK_WIDTH, BRICK_HEIGHT)
   BALL_RADIUS     = 10
   BRICK_Y_OFFSET  = 70
   TURNS           = 3
@@ -47,6 +72,7 @@ class Breakout:
     self.surface = pygame.display.set_mode((400, 600))
     self.surface.fill((255, 255, 255))
     pygame.display.set_caption('Hello World')
+    self.clock = pygame.time.Clock()
 
   def make_bricks(self):
     bricks = []
@@ -61,14 +87,28 @@ class Breakout:
     return pygame.sprite.Group(bricks)
 
   def run(self):
-    bricks = self.make_bricks()
+    self.bricks = self.make_bricks()
+    ball = Ball(Breakout.WIDTH / 2, Breakout.HEIGHT / 2, Breakout.BALL_RADIUS)
+    self.balls = pygame.sprite.Group([ball])
     while True:
       for event in pygame.event.get():
         if event.type == QUIT:
           pygame.quit()
 
           sys.exit()
-      bricks.draw(self.surface)
+      self.surface.fill((255, 255, 255))
+      self.bricks.draw(self.surface)
+      self.balls.draw(self.surface)
+      self.balls.update()
+
+      for ball in self.balls.sprites():
+        if ball.rect.x >= Breakout.WIDTH or ball.rect.x <= 0:
+          ball.bounceX()
+
+        if ball.rect.y >= Breakout.HEIGHT or ball.rect.y <= 0:
+          ball.bounceY()
+
       pygame.display.flip()
+      self.clock.tick(60)
 
 Breakout().run()
